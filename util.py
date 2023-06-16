9b7681b0e445f44f9f0a5cae8c437285e0075d5b
ToKliar <1224769259@qq.com>
2023-06-16 17:32:08 +0800

    code fix

--- util.py
+++ util.py
@@ -1,5 +1,4 @@
 def recommend(model, prompt):
-    print(prompt)
     messages = [{"role": "user", "content": prompt}]
     response = model.chat_completion(messages, verbose=False, streaming=False)
     response = response['choices'][0]['message']['content']
